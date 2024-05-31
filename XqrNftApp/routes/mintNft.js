92594d7a7fde3d06e535b480084d76e6920bb54f
snow-saboten <snow.saboten74@gmail.com>
2024-05-31 21:24:07 +0900

    画面入力値をもとにNFTをミントする

--- XqrNftApp/routes/mintNft.js
+++ XqrNftApp/routes/mintNft.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
 
 const xrpl = require('xrpl');
 
@@ -17,8 +21,9 @@ router.get('/', function(req, res, next) {
 router.post('/mint', async function(req,res,next) {
   console.log('start');
 
-  const collectionName = req.body.collname;
-  const jsonUri = 'https://orange-official-gecko-137.mypinata.cloud/ipfs/QmRpu7AP1PFCySyvhm3gSJZxvHjTve5scHavB5r5itpDwX';
+  const bodyData = req.body;
+
+  const jsonUri = bodyData.json_uri;
   let outputMsg = '';
 
   const net = 'wss://s.altnet.rippletest.net:51233';
@@ -26,7 +31,7 @@ router.post('/mint', async function(req,res,next) {
   outputMsg += 'connecting to' + net + '....';
   
 
-  
+  const system_wallet = xrpl.Wallet.fromSeed('sEd7wQbKfXydEcNLtLViMH8TSCUv2fm');
   const client = new xrpl.Client(net);
   await client.connect();
   outputMsg += '\nConnected. Minting NFT.';
@@ -34,34 +39,45 @@ router.post('/mint', async function(req,res,next) {
 
 
 
-  // const transationJson = {
-  //   "TransactionType": "NFTokenMint",
-  //   "Account": "rPxR3CeKzJzcqtnbsyTdYpLrAHmd7fFwiq",
-  //   "URI": xrpl.convertStringToHex(jsonUri),
-  //   "Flags": 8,
-  //   "TransferFee": 320,
-  //   "NFTokenTaxon": 0 
-  // }
+  const transationJson = {
+    "TransactionType": "NFTokenMint",
+    "Account": "rPxR3CeKzJzcqtnbsyTdYpLrAHmd7fFwiq",
+    "URI": xrpl.convertStringToHex(jsonUri),
+    "Flags": 8,
+    "TransferFee": bodyData.loyalty * 1000, // x * 1000
+    "NFTokenTaxon": 0 ,
+    "OtherData": {
+      "CollectionNo": bodyData.collno,
+      "CollectionName": bodyData.collname,
+      "NFTName": bodyData.nftname,
+      "Description": bodyData.description
+    }
+  }
 
-//   const tx = await client.submitAndWait(transationJson, {wallet: system_wallet});
+  const tx = await client.submitAndWait(transationJson, {wallet: system_wallet});
   
-// console.log(tx.result.meta.TransactionResult);
+console.log(tx.result.meta.TransactionResult);
 
-//   const nfts = await client.request({
-//     method: "account_nfts",
-//     account: system_wallet.classicAddress
-//   });
+  const nfts = await client.request({
+    method: "account_nfts",
+    account: system_wallet.classicAddress
+  });
 
-//   outputMsg += '\n\nTransaction result: ' + tx.result.meta.TransactionResult;
+  outputMsg += '\n\nTransaction result: ' + tx.result.meta.TransactionResult;
+  outputMsg += '\n' + nfts;
+  console.log(nfts);
 
-  console.log(collectionName);
+  
   client.disconnect();
 
-  res.render('./mintNft.ejs', {
+  res.render('mintNft',{
     walletAddress: walletAddress,
     outputMsg: outputMsg
   });
 });
+
+
+
 
 
 
