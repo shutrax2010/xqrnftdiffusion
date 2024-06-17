cb4d625dac3ebe50a770a4380acdc0bd17fc6f2a
Phyo <1997kophyoe@gmail.com>
2024-06-17 11:02:22 +0900

    update authentication middleware

--- XqrNftApp/authMiddleware.js
+++ XqrNftApp/authMiddleware.js
@@ -1,6 +1,6 @@
 const isAuthenticated = (req, res, next) => {
     // Example: Check if user is authenticated 
-    if (req.session && req.session.authenticated) {
+    if (req.session && req.session.authenticated && req.session.resolved) {
       return next(); // User is authenticated, proceed to next middleware or route handler
     } else {
       return res.redirect('/'); // Redirect to index or login page if not authenticated
