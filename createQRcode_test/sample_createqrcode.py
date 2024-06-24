d393296d8adfa77cd47d060de82098318bc2ea77
Najimin <meganemgn2@gmail.com>
2024-06-24 21:29:58 +0900

    jsonからQR作成リファクタ

--- createQRcode_test/sample_createqrcode.py
+++ createQRcode_test/sample_createqrcode.py
@@ -2,38 +2,37 @@ import os
 import requests
 import io
 import qrcode
-from PIL import Image
 import json
 from flask import Flask, send_file, render_template, request, jsonify
 from flask_cors import CORS
-app = Flask(__name__)
+app = Flask(__name__)	
 CORS(app)  # CORSを全てのエンドポイントに対して有効にする
 
 @app.route('/', methods=['POST'])
 def index():
-    name = request.get_json()
-    img = qrcode.make(name)
-    img_byte_array = io.BytesIO()
-    img.save(img_byte_array, format='PNG')
-    img_byte_array.seek(0)   
-    img.save('test_createQRcode.png')
-    
-    with open('test_createQRcode.png', "rb") as q:
-        url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
-        files = [
-        ('file', ('test_createQRcode.png',q )),
-        ]
-        headers = {
-        'pinata_api_key': "ef4b2776bda587ee59df",
-        'pinata_secret_api_key': "668688893b3ee64dcfdcc2b8ba801ecda95d916fdc307bd21f8d8351be86940c"
-        }
-        response = requests.request("POST", url, files=files, headers=headers)
-        print(response.text)
-        
-        q.close()
-    os.remove('test_createQRcode.png')
-    
-    getUrl = 'https://gateway.pinata.cloud/ipfs/' + response.json()["IpfsHash"]
+	json_data = request.form['jsonData']
+	json_data = json.loads(json_data)  # JSON文字列をPythonオブジェクトに変換
+	
+	qr_data = json.dumps(json_data)  # JSONデータを文字列に変換してQRコードのデータとする
+	qr = qrcode.make(qr_data)
+	
+	img_byte_array = io.BytesIO()
+	img.save(img_byte_array, format='PNG')
+	img_byte_array.seek(0)
+	
+	# IPFSへのファイルのピン留め（アップロード）
+	url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+	files = {'file': ('qrcode.png', img_byte_array, 'image/png')}
+	headers = {
+		'pinata_api_key': "7889b920748ef631b2b0",
+		'pinata_secret_api_key': "d8f3b545743f4760ec2937d3faf4df60b481a28b9b7d1c83c16ba72685455db4"
+	}
+	response = requests.post(url, files=files, headers=headers)
+	
+	# 一時ファイルを削除
+	img_byte_array.close()
+	
+	getUrl = 'https://gateway.pinata.cloud/ipfs/' + response.json()["IpfsHash"]
     return getUrl
         
 if __name__ == '__main__':
